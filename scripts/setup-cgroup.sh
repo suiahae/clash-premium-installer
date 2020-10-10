81e03864d046e4f69b8dadd590c681fc8afb4143
suiahae <45323123+suiahae@users.noreply.github.com>
2020-10-11 00:16:25 +0800

    fix: Add "touch" command to enhance compatibility

--- scripts/setup-cgroup.sh
+++ scripts/setup-cgroup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# https://www.kernel.org/doc/Documentation/cgroup-v1/net_cls.txt
 
 PROXY_BYPASS_CGROUP="0x16200000"
 
@@ -8,4 +9,5 @@ fi
 
 mkdir -p /sys/fs/cgroup/net_cls/bypass_proxy
 echo "$PROXY_BYPASS_CGROUP" > /sys/fs/cgroup/net_cls/bypass_proxy/net_cls.classid
+touch /sys/fs/cgroup/net_cls/bypass_proxy/tasks
 chmod 666 /sys/fs/cgroup/net_cls/bypass_proxy/tasks
